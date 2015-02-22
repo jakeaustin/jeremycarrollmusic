33a2afa88a1099e8f026358df48ad9e5218fe9c8
Jake Austin <jakeaust07@gmail.com>
2015-02-22 16:33:02 -0500

    order posts by date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
   respond_to :json, :html
 
   def index
-    @musicPosts = Post.where("category = 'Music'")
-    @lessonPosts = Post.where("category = 'Lesson'")
+    @musicPosts = Post.where("category = 'Music'").order(created_at: :desc)
+    @lessonPosts = Post.where("category = 'Lesson'").order(created_at: :desc)
     @posts = {musicPosts: @musicPosts, lessonPosts: @lessonPosts}
     respond_with @posts
   end
