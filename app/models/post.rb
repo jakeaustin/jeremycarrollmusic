fe816ff95f6696980a53597002dbaef62ef963de
Jake Austin <jakeaust07@gmail.com>
2015-02-24 18:08:48 -0500

    define tag and tagging

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-
+  has_many :tags, through: :taggings
   validates :title, :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222205637) do
+ActiveRecord::Schema.define(version: 20150224230114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20150222205637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "category"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
