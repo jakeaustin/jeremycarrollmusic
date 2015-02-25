a0e6f27e3bb306afe7ad1b46ee4e8cf4865b16c0
Jake Austin <jakeaust07@gmail.com>
2015-02-24 19:10:24 -0500

    defined tag#index and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :user, :path => '', :path_names => { :sign_in => "admin"}
   root 'home#index'
   resources :posts
+  resources :tags, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
